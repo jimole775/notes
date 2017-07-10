9983085a6e5350d115827b7fd54bd2aee633a189
Andy-Rong <727346595@qq.com>
2017-07-11 00:16:08 +0800

    image-recognition

--- practise/image_recognition/run.js
+++ practise/image_recognition/run.js
@@ -3,5 +3,10 @@
  */
 global.ROOT_DIR = __dirname;
 global.HOST_DIR = __dirname;
-var createHttp = require("./http.js");
+var createHttp = require("./http_basic/index.js");
 createHttp.run();
+
+var filesIO = require("./files_store");
+filesIO.extractFile("CDN.txt", function (buf,err) {
+    console.log(buf.toString("utf8"))
+});
