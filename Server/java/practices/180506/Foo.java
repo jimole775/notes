f5a974132f3c2f6d9fbbdbe78e5b09824c6266f2
andy-rong <727346595@qq.com>
2018-05-07 21:40:49 +0800

    IDE setting

---
+++ Server/java/practices/180506/Foo.java
@@ -0,0 +1,38 @@
+/**
+ * Foo
+ */
+
+
+public class Foo{
+    public static void main(String[] args){
+        Test test = new Test();
+        TestFormula formula = new TestFormula();
+        test.name();
+        System.out.println("let's say hello!" + formula.trydo(9));
+    } 
+}
+
+class Test{
+    public void name() {
+        System.out.println("apply name method!");
+    }   
+}
+
+interface Formula {
+    int calc();
+    default int trydo(int a){
+        return a * 10;
+    }
+}
+
+class TestFormula implements Formula{
+    @Override
+    int trydo(int a){
+        return a;
+    }
+
+    // @Override
+    String calc() {
+        return "run calc!";
+    }
+}
