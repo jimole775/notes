80383a9fb323c1c10376569c2fbde4239b836406
andy-rong <727346595@qq.com>
2018-07-24 21:33:57 +0800

    daily

--- Server/java/practices/DoMain.java
+++ Server/java/practices/DoMain.java
@@ -3,7 +3,10 @@ import java.util.*;
 import java.util.logging.*;
 import java.io.InputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.File;
+import java.util.zip.ZipEntry;
+
 
 public class DoMain{
     public static void main(String[] arg){
@@ -12,17 +15,30 @@ public class DoMain{
 
     }
 
-    static void doParam(Integer... its){   
+    static void doParam(int... its){   
         try{
-        Logger log = Logger.getLogger("com.packsomething.DoMain");
-        File direction = new File("");
-        File file = new File(direction.getAbsolutePath() + File.separator + "errs.log");
-        FileInputStream st = new FileInputStream(file);
-        byte[] b = new byte[1024];
-        st.read(b);
-        st.close();
-        log.info(new String(b));
-    }catch(Exception e){}
+            Logger log = Logger.getLogger("com.packsomething.DoMain");
+            File direction = new File("");
+            File file = new File(direction.getAbsolutePath() + File.separator + "errs.log");
+
+            FileOutputStream wt = new FileOutputStream(file.getAbsolutePath());
+            StringBuilder strs = new StringBuilder("im ");
+            strs = strs.append("write ");
+            strs = strs.append("down ");
+            strs = strs.append("here ");
+            strs = strs.append("!\n");
+            byte[] b = strs.toString().getBytes("GBK");
+            
+
+            wt.write(b);
+            wt.write(b);
+            wt.close();
+            log.info(new String(b));
+
+
+        }catch(Exception e){
+
+        }
     }
 }
 
