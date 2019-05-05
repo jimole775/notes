50c14c4d0792ff4784da1045743fb9b6e1eb6eda
jimole775 <727346595@qq.com>
2019-05-05 09:49:55 +0800

    live

--- module1.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Andy on 2017/10/4.
- */
-function print(){
-    console.log("hello you");
-    let foo = "bar";
-}
-if(process.env.NODE_ENV)console.log(process.env.NODE_ENV);
-// export default print;
