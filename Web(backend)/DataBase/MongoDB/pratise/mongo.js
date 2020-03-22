660c37f3e4c30aa0a2c2d659708f55600a44f9e6
Andy-Super <727346595@163.com>
2020-03-23 00:22:26 +0800

    update

--- Web(backend)/DataBase/MongoDB/pratise/mongo.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Andy-Rong on 2017/7/14.
- */
-var mongo = require("mongodb");
-var server = new mongo.Server("localhost",27017,{auto_reconnect:true});
-var db = new mongo.Db("foo",server,{safe:true});
