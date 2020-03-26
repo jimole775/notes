83a5bf455e7061d049a0556f2d9bdf90af58d2b7
rongchuanda <rongchuanda@chinasie.com>
2020-03-26 18:13:36 +0800

    对Promise的新记录

--- Languages/Javascript/ES6特性/Promise/ES5版本原生实现.js
+++ Languages/Javascript/ES6特性/Promise/ES5版本原生实现.js
@@ -1,41 +1,86 @@
-function Promise(callback) {
-    var that = this;
-    that.value = null;
-    var resolve = function (value) {
-        return that.value = value;
-    };
-    var reject = function (err) {
+function MyPromise(bootStrap) {
+  this.taskResponse = null
+  this.taskErrInfo = null
+  this.taskQueue = [] // 任务队列，由 'this.then' 被调用的时候存储
 
-    };
-    callback(resolve, reject);
+  // 由于ES5函数内部this的指向问题，
+  // 这里必须先固定指向MyPromise内部
+  // 否则 solve 或 reject 被调用的时候，this将指向global
+  var that = this
 
-    that.then = function (handler) {
-        var walker = setInterval(function () {
-            if (that.value) {
-                clearInterval(walker);
-                that.value = handler(that.value) || that.value;
-            }
-        }, 15);
-        return that;
-    };
+  // taskQueue队列调用的开关
+  var solve = function(response) {
+    that.taskResponse = response
+    that.callTasks(that.taskResponse)
+  }
 
-    return that;
+  // 异常处理的开关
+  var reject = function(response) {
+    that.taskErr = response
+    that.errHandler(that.taskErr)
+  }
+
+  // 默认处理异常的函数
+  this.errHandler = function() {
+    throw this.taskErrInfo
+  }
+
+  // 默认终点的执行函数
+  this.finallyHandler = function() {}
+
+  bootStrap && bootStrap(solve, reject)
 }
 
+MyPromise.prototype.callTasks = function(taskResponse) {
+  try {
+    // 直接执行 then 存储的任务
+    const nextResponse = this.taskQueue.shift()(taskResponse)
+    if (this.taskQueue.length) {
+      this.callTasks(nextResponse || taskResponse)
+    } else if (this.finallyQueue.length) {
+      this.finallyHandler(nextResponse || taskResponse)
+    }
+  } catch (error) {
+    this.errHandler(error)
+  }
+}
 
-var promise = new Promise(function (resolve, reject) {
-    var res = null;
-    setTimeout(function () {
-        res = "hello";
-        resolve(res);
-    }, 3000);
-});
+MyPromise.prototype.then = function(task) {
+  this.taskQueue.push(task)
+  return this
+}
+
+MyPromise.prototype.catch = function(errHandler) {
+  this.errHandler = errHandler
+  return this
+}
+
+MyPromise.prototype.finally = function(finallyHandler) {
+  this.finallyHandler = finallyHandler
+  return this
+}
+
+// usage:
+// 输出 'then1: 1  then2: 2  finally: 3'
+let tick = 0
+const promise = new MyPromise(function(s, j) {
+  setTimeout(() => {
+    s(++tick)
+  }, 1000)
+})
 
 promise
-    .then(function (result) {
-        console.log(result);
-        return "world";
-    })
-    .then(function (result) {
-        console.log(result)
-    });
+  .then(data => {
+    console.log("then1:", data)
+    return data + 1
+  })
+  .then(data => {
+    console.log("then2:", data)
+    return data + 1
+  })
+  .catch(data => {
+    console.log("err:", data)
+  })
+  .finally(data => {
+    console.log("finally:", data)
+  })
