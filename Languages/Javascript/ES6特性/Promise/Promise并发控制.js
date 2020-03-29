30db0db429c8ab1080278b3b8c45ebfbbf52945e
Andy-Super <727346595@163.com>
2020-03-29 19:15:10 +0800

    【笔记】--- js'线程'并发的实现

--- Languages/Javascript/ES6特性/Promise/Promise并发控制.js
+++ Languages/Javascript/ES6特性/Promise/Promise并发控制.js
@@ -1,32 +1,45 @@
 let taskLiving = 0
-let taskQueue = [] // 存储队列
+let taskQueue = [] // 任务队列
 let max = 5
 
-function bunch(task) {
-  // 任务多出限制，存储
+/**
+ * 【任务收集器】，只做两件事：
+ * 1. 发起5个'线程'，每个'线程'分别执行自己的[task]
+ * 2. 超过5个'线程'之后，把后面的任务存储起来
+ **/
+function threadManager(task) {
   if (taskLiving >= max) {
     taskQueue.push(task)
   } else {
-    // 任务少于限制，调用
-    promiseEmit(task)
+    thread(task)
   }
   taskLiving ++
 }
 
-function promiseEmit(task) {
+/**
+ * 【任务触发器】，只做一件事：
+ * 递归自己，反复调用任务队列，直到任务队列为空
+ * 说明：
+ * 因为线程管理器[threadManager]已经发起了5个'线程'，
+ * 每个 [thread] 执行完一个任务后，只负责递归自己就好了，
+ * 这样就可以保持同时5个'线程'的任务在跑
+ **/
+function thread(task) {
   return new Promise((s, j) => { task(s, j) })
-  .finally(() => { // 以finally方法，当作并发异步任务的状态观测点
+  .finally(() => { // 以finally方法，当作下一批任务的连接点
     taskLiving --
     if (taskQueue.length > 0) {
       // 递归操作
-      promiseEmit(taskQueue.shift())
+      thread(taskQueue.shift())
     }
+  }).catch((err) => {
+    console.log(err)
   })
 }
 
 let loop = 10
 while(loop--) {
-  bunch((s, j) => {
+  threadManager((s, j) => {
     setTimeout(() => {
       console.log('running task')
       s()
