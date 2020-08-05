96ac53ba52fae53ead7bac298789abefeabc5efc
Rongxis <727346595@qq.com>
2020-08-05 16:39:02 +0800

    Update utils.js

--- web开发/web前端/第三方类库/utils.js
+++ web开发/web前端/第三方类库/utils.js
@@ -101,48 +101,11 @@ export function fixedNumber(numberOrStr) {
   }
 }
 
-/** 修饰钱类型的数值的位数
- * @param numberOrStr [String|Number]
- * @param decimal [Number]
- * @return [String]
- * @template = formatMoney(0.0512) => 0.05
- * @template = formatMoney(100000.0512) => 100,000.05
- */
-export function formatMoney(numberOrStr, decimal = 2) {
-  // 空类型不处理
-  if (numberOrStr === null || numberOrStr === undefined || numberOrStr === '') return ''
-  // 保留原数字，用作正负数的标记
-  const originNumber = Number.parseFloat(fixedNumber(numberOrStr)(decimal))
-  // 非数字类型不处理
-  if (Number.isNaN(originNumber)) return ''
-  // 计算用绝对值
-  const str = Math.abs(originNumber) + ''
-  const intStr = str.split('.')[0] || '' // 保存整数位
-  const decimalStr = str.split('.')[1] || '' // 保存小数位
-  if (intStr.length < 5) return str
-  const res = []
-  let step = 3
-  let i = intStr.length
-  while (i--) {
-    if (step === 0) {
-      step = 3
-      res.unshift(',')
+export function formatMoney(value) {
+    if (value === null || value === undefined) {
+      value = ''
     }
-    step--
-    res.unshift(intStr[i])
-  }
-  // 处理负数，如果是负数，在首位增加 负号
-  if (originNumber < 0) {
-    res.unshift('-')
-  }
-
-  // 处理小数位
-  const decimalArr = []
-  if (decimalStr.length) {
-    decimalArr.push('.')
-    decimalArr.push(decimalStr)
-  }
-  return res.concat(decimalArr).join('')
+    return `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
 
 /** 把对象转换成的urlquery字段
