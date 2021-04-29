51cd5aea62311984a20b97676fe90b5b45794239
16603013472 <rongchuanda@chinasie.com>
2021-04-29 14:38:22 +0800

    updates

--- 语言基础/TypeScript/知识节点/语法/mixin.ts
+++ 语言基础/TypeScript/知识节点/语法/mixin.ts
@@ -2,12 +2,11 @@
  * Created by Andy on 2017/8/29.
  */
 
-//混合继承，当然可以继承抽象类，也可以继承接口
-
+// 混合继承，当然可以继承抽象类，也可以继承接口
 class Cat {
-    name:string;
-    age:number;
-    cry<T,U>(name:T,age:U):any{
-        return {name,age};
-    };
+    name: string
+    age: number
+    cry<T, U>(name: T, age: U): any {
+        return { name, age }
+    }
 }
