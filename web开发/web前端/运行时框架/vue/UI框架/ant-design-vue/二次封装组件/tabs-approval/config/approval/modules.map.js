1f3a8e9fd8101a9d6be6db0b6787b4660bbe44a2
16603013472 <rongchuanda@chinasie.com>
2021-05-27 15:44:19 +0800

    refactor: 去掉封装组件，实例不应该存放在这里，体积太大

--- web开发/web前端/运行时框架/vue/UI框架/ant-design-vue/二次封装组件/tabs-approval/config/approval/modules.map.js
+++
@@ -1,6 +0,0 @@
-import { demandModulesMap } from './modules/tab0'
-import { changeModulesMap } from './modules/tab3'
-export const approvalModulesMap = {
-  0: demandModulesMap,
-  3: changeModulesMap
-}
